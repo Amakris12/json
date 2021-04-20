9f6f5607b866dcbf4d2c4c78391c05d6851ca39c
Amakris12 <71471809+Amakris12@users.noreply.github.com>
2021-04-19 20:21:04 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+let cats = {
+  "title": "Cat Families",
+  "intro": "This is a list of cats and their mother that we are breeding. If you are interested in adopting please contact us",
+  "cats": [
+    {
+      "name": "Lindy",
+      "breed": "Cymric",
+      "color": "white",
+      "kittens": [
+        {
+          "name": "Percy",
+          "gender": "m"
+        },
+        {
+          "name": "Thea",
+          "gender": "f"
+        },
+        {
+          "name": "Annis",
+          "gender": "f"
+        }
+      ]
+    },
+    {
+      "name": "Mina",
+      "breed": "Aphrodite Giant",
+      "color": "ginger",
+      "kittens": [
+        {
+          "name": "Doris",
+          "gender": "f"
+        },
+        {
+          "name": "Pickle",
+          "gender": "f"
+        },
+        {
+          "name": "Max",
+          "gender": "m"
+        }
+      ]
+    },
+    {
+      "name": "Antonia",
+      "breed": "Ocicat",
+      "color": "leopard spotted",
+      "kittens": [
+        {
+          "name": "Bridget",
+          "gender": "f"
+        },
+        {
+          "name": "Randolph",
+          "gender": "m"
+        }
+      ]
+    }
+  ]
+}
+var cat = JSON.stringify(Object.fromEntries(cats.cats));
+console.log(cat)
+
+ 
